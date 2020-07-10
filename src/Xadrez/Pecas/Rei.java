708c6435147da75afd5263baabe2c84e8cff4955
ReiJrMed <reijrspy@gmail.com>
2020-07-10 12:05:35 -0300

    Implementou-se os movimentos da classe Rei

--- src/Xadrez/Pecas/Rei.java
+++ src/Xadrez/Pecas/Rei.java
@@ -2,6 +2,7 @@ package Xadrez.Pecas;
 
 import Xadrez.Cor;
 import Xadrez.Peca_Xadrez;
+import tabuleiroGame.Position;
 import tabuleiroGame.Tabuleiro;
 
 public class Rei extends Peca_Xadrez{
@@ -14,10 +15,58 @@ public class Rei extends Peca_Xadrez{
 	public final String toString() {
 		return "r";
 	}
+	
+	private boolean podeMover(Position position) {
+		Peca_Xadrez p = (Peca_Xadrez) getTabuleiro().peca(position);
+		return ((p == null) || (p.getCor() != getCor()));
+	}
 
 	@Override
 	public boolean[][] movimentosPossiveis() {
 		boolean[][] mat = new boolean[getTabuleiro().getLinha()][getTabuleiro().getColuna()];
+		
+		Position p = new Position(0, 0);
+		
+		//acima
+		p.setValores(position.getLinha() -1, position.getColuna());
+		if((getTabuleiro().existPosition(p)) && (podeMover(p)))
+			mat[p.getLinha()][p.getColuna()] = true;
+		
+		//abaixo
+		p.setValores(position.getLinha() +1, position.getColuna());
+		if((getTabuleiro().existPosition(p)) && (podeMover(p)))
+		   mat[p.getLinha()][p.getColuna()] = true;
+	
+		//esquerda
+		p.setValores(position.getLinha(), position.getColuna() -1);
+		if((getTabuleiro().existPosition(p)) && (podeMover(p)))
+			mat[p.getLinha()][p.getColuna()] = true;
+		
+		//direita
+		p.setValores(position.getLinha(), position.getColuna() +1);
+		if((getTabuleiro().existPosition(p)) && (podeMover(p)))
+			mat[p.getLinha()][p.getColuna()] = true;
+		
+		//Noroeste
+		p.setValores(position.getLinha() -1, position.getColuna() -1);
+		if((getTabuleiro().existPosition(p)) && (podeMover(p)))
+			mat[p.getLinha()][p.getColuna()] = true;
+		
+		//Nordeste
+		p.setValores(position.getLinha() -1, position.getColuna() +1);
+		if((getTabuleiro().existPosition(p)) && (podeMover(p)))
+			mat[p.getLinha()][p.getColuna()] = true;
+		
+		//Sudoeste
+		p.setValores(position.getLinha() +1, position.getColuna() -1);
+		if((getTabuleiro().existPosition(p)) && (podeMover(p)))
+			mat[p.getLinha()][p.getColuna()] = true;
+		
+		//sudeste
+		p.setValores(position.getLinha() +1, position.getColuna() +1);
+		if((getTabuleiro().existPosition(p)) && (podeMover(p)))
+			mat[p.getLinha()][p.getColuna()] = true;
+						
 		return mat;
 	}
 
--- src/Xadrez/Pecas/Torre.java
+++ src/Xadrez/Pecas/Torre.java
@@ -24,39 +24,60 @@ public class Torre extends Peca_Xadrez{
 		
 		//acima
 		p.setValores(position.getLinha() -1, position.getColuna());
-		while((getTabuleiro().existPosition(p)) && (!getTabuleiro().positionOcupada(p))) {
-			mat[p.getLinha()][p.getColuna()] = true;
+		while(getTabuleiro().existPosition(p)) {
+			if((!getTabuleiro().positionOcupada(p))) {
+				  mat[p.getLinha()][p.getColuna()] = true;
+				} else {
+					if(existePecaAdversaria(p)) {
+						mat[p.getLinha()][p.getColuna()] = true;
+					}
+				  break;
+				}
 			p.setLinha(p.getLinha() -1);
 		}
-		if((getTabuleiro().existPosition(p)) && (getTabuleiro().positionOcupada(p)) && (existePecaAdversaria(p)))
-			mat[p.getLinha()][p.getColuna()] = true;
 		
 		//esquerda
 		p.setValores(position.getLinha(), position.getColuna() -1);
-		while((getTabuleiro().existPosition(p)) && (!getTabuleiro().positionOcupada(p))) {
-			mat[p.getLinha()][p.getColuna()] = true;
+		while(getTabuleiro().existPosition(p)) {
+			if((!getTabuleiro().positionOcupada(p))) {
+				  mat[p.getLinha()][p.getColuna()] = true;
+				} else {
+					if(existePecaAdversaria(p)) {
+						mat[p.getLinha()][p.getColuna()] = true;
+					}
+				  break;
+				}
 			p.setColuna(p.getColuna() -1);
 		}
-		if((getTabuleiro().existPosition(p)) && (getTabuleiro().positionOcupada(p)) && (existePecaAdversaria(p)))
-			mat[p.getLinha()][p.getColuna()] = true;
-		
+				
 		//direita
 		p.setValores(position.getLinha(), position.getColuna() +1);
-		while((getTabuleiro().existPosition(p)) && (!getTabuleiro().positionOcupada(p))) {
-			mat[p.getLinha()][p.getColuna()] = true;
+		while(getTabuleiro().existPosition(p)) {
+			if((!getTabuleiro().positionOcupada(p))) {
+				  mat[p.getLinha()][p.getColuna()] = true;
+				} else {
+					if(existePecaAdversaria(p)) {
+						mat[p.getLinha()][p.getColuna()] = true;
+					}
+				  break;
+				}
 			p.setColuna(p.getColuna() +1);
-		}
-		if((getTabuleiro().existPosition(p)) && (getTabuleiro().positionOcupada(p)) && (existePecaAdversaria(p)))
-			mat[p.getLinha()][p.getColuna()] = true;
+		}		
 		
 		//acima
 		p.setValores(position.getLinha() +1, position.getColuna());
-		while((getTabuleiro().existPosition(p)) && (!getTabuleiro().positionOcupada(p))) {
-			mat[p.getLinha()][p.getColuna()] = true;
+		while(getTabuleiro().existPosition(p)) {
+			if((!getTabuleiro().positionOcupada(p))) {
+			  mat[p.getLinha()][p.getColuna()] = true;
+			} else {
+				if(existePecaAdversaria(p)) {
+					mat[p.getLinha()][p.getColuna()] = true;
+				}
+			  break;
+			}
 			p.setLinha(p.getLinha() +1);
 		}
-		if((getTabuleiro().existPosition(p)) && (getTabuleiro().positionOcupada(p)) && (existePecaAdversaria(p)))
-			mat[p.getLinha()][p.getColuna()] = true;
+		
 		
 		return mat;
 	}
